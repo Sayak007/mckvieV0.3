7079fb94997b93153e473a3b9adbd5a4dce57cda
tclubmckvie <tclubmckvie@gmail.com>
2019-07-22 22:27:37 +0800

    Syllabus more or less fixed..theres problem in the links

--- app/src/main/java/com/techclub/mckvie/Syllabus.java
+++ app/src/main/java/com/techclub/mckvie/Syllabus.java
@@ -1,14 +1,20 @@
 package com.techclub.mckvie;
 
 import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.Uri;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Syllabus extends AppCompatActivity {
+    long downloadID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,102 +43,110 @@ public class Syllabus extends AppCompatActivity {
         button1st.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                DownloadManager downloadManager =(DownloadManager)getSystemService(DOWNLOAD_SERVICE);
-                Uri uri = Uri.parse("http://www.mckvie.edu.in/site/assets/files/1161/1st_year_b_tech_syllabus_revised_18_08_10.pdf");
-                DownloadManager.Request request=new DownloadManager.Request(uri);
-                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-                downloadManager.enqueue(request);
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.mckvie.edu.in/site/assets/files/1161/1st_year_b_tech_syllabus_revised_18_08_10.pdf"));
+                startActivity(intent);
             }
         });
 
         buttonCSE.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                DownloadManager downloadManager =(DownloadManager)getSystemService(DOWNLOAD_SERVICE);
-                Uri uri = Uri.parse("http://www.wbut.ac.in/syllabus/CSE_Final_Upto_4h_Year%20Syllabus_14.03.14.pdf");
-                DownloadManager.Request request=new DownloadManager.Request(uri);
-                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-                downloadManager.enqueue(request);
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.wbut.ac.in/syllabus/CSE_Final_Upto_4h_Year%20Syllabus_14.03.14.pdf"));
+                startActivity(intent);
             }
         });
 
         buttonAUE.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                DownloadManager downloadManager =(DownloadManager)getSystemService(DOWNLOAD_SERVICE);
-                Uri uri = Uri.parse("http://www.mckvie.edu.in/site/assets/files/1161/aue_final_upto_4th_year-syllabus_05_06_13.pdf");
-                DownloadManager.Request request=new DownloadManager.Request(uri);
-                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-                downloadManager.enqueue(request);
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.mckvie.edu.in/site/assets/files/1161/aue_final_upto_4th_year-syllabus_05_06_13.pdf"));
+                startActivity(intent);
             }
         });
 
         buttonME.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                DownloadManager downloadManager =(DownloadManager)getSystemService(DOWNLOAD_SERVICE);
-                Uri uri = Uri.parse("http://www.mckvie.edu.in/site/assets/files/1161/me_final_upto_4th_year-syllabus_04_06_13.pdf");
-                DownloadManager.Request request=new DownloadManager.Request(uri);
-                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-                downloadManager.enqueue(request);
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.mckvie.edu.in/site/assets/files/1161/me_final_upto_4th_year-syllabus_04_06_13.pdf"));
+                startActivity(intent);
             }
         });
+        buttonECE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.mckvie.edu.in/site/assets/files/1161/ece_final_upto_4th_year-syllabus_04_06_13.pdf"));
+                startActivity(intent);
+            }
+        });
+        buttonEE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.mckvie.edu.in/site/assets/files/1161/ee_final_upto_4th_year-syllabus_05_09_13.pdf"));
+                startActivity(intent);
+            }
+        });
+        buttonIT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.mckvie.edu.in/site/assets/files/1161/it_final_upto_4th_year-syllabus_27_07_13.pdf"));
+                startActivity(intent);
+            }
+        });
+
 
         MTECHECE.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                DownloadManager downloadManager =(DownloadManager)getSystemService(DOWNLOAD_SERVICE);
-                Uri uri = Uri.parse("http://www.mckvie.edu.in/site/assets/files/1161/mtech_ececommunication_comm_detail_syllabus_2010.pdf");
-                DownloadManager.Request request=new DownloadManager.Request(uri);
-                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-                downloadManager.enqueue(request);
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.mckvie.edu.in/site/assets/files/1161/mtech_ececommunication_comm_detail_syllabus_2010.pdf"));
+                startActivity(intent);
             }
         });
 
         MTECHCSE.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                DownloadManager downloadManager =(DownloadManager)getSystemService(DOWNLOAD_SERVICE);
-                Uri uri = Uri.parse("http://www.wbut.ac.in/syllabus/M.Tech_CSE_IT_Unified_19.02.14_2.pdf");
-                DownloadManager.Request request=new DownloadManager.Request(uri);
-                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-                downloadManager.enqueue(request);
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.wbut.ac.in/syllabus/M.Tech_CSE_IT_Unified_19.02.14_2.pdf"));
+                startActivity(intent);
             }
         });
 
         MTECHAE.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                DownloadManager downloadManager =(DownloadManager)getSystemService(DOWNLOAD_SERVICE);
-                Uri uri = Uri.parse("http://www.mckvie.edu.in/site/assets/files/1161/automotive-technology-syllabus-mckvie.pdf");
-                DownloadManager.Request request=new DownloadManager.Request(uri);
-                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-                downloadManager.enqueue(request);
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.mckvie.edu.in/site/assets/files/1161/automotive-technology-syllabus-mckvie.pdf"));
+                startActivity(intent);
             }
         });
 
         MTECHVLSI.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                DownloadManager downloadManager =(DownloadManager)getSystemService(DOWNLOAD_SERVICE);
-                Uri uri = Uri.parse("http://www.mckvie.edu.in/site/assets/files/1161/mtech_ecemicroelectronics_vlsi-designs_comm_detail_syllabus_2010.pdf");
-                DownloadManager.Request request=new DownloadManager.Request(uri);
-                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-                downloadManager.enqueue(request);
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.mckvie.edu.in/site/assets/files/1161/mtech_ecemicroelectronics_vlsi-designs_comm_detail_syllabus_2010.pdf"));
+                startActivity(intent);
             }
         });
 
         MCA.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                DownloadManager downloadManager =(DownloadManager)getSystemService(DOWNLOAD_SERVICE);
-                Uri uri = Uri.parse("http://www.mckvie.edu.in/site/assets/files/1161/mca_new_syllabus.pdf");
-                DownloadManager.Request request=new DownloadManager.Request(uri);
-                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-                downloadManager.enqueue(request);
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.mckvie.edu.in/site/assets/files/1161/mca_new_syllabus.pdf"));
+                startActivity(intent);
             }
         });
     }
+
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
